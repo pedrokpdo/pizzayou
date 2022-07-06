de7bebff8fc3594e2f527c4250749f6a57adaa8f
pedrokpdo <pedromotoehm@gmail.com>
2022-07-06 00:04:21 -0300

    first step app

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { Container, Title } from './styles'
+
+export function SignIn() {
+
+    return (
+        <Container>
+            <View>
+                <Title>ola mundo</Title>
+            </View>
+        </Container>
+    )
+}
